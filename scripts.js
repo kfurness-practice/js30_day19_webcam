9795b56670575d202efba34f14a22a1e401d1f88
kelly <kelly.n.waite@gmail.com>
2017-01-18 13:18:28 -0700

    Access camera and paint camera video to canvas

--- scripts.js
+++ scripts.js
@@ -3,3 +3,27 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false})
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch( err => { alert("We do not have access to your camera!")} )
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  // return setInterval in case you ever need to stop/change it, you have access to it!
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height )
+  }, 16);
+}
+
+getVideo();
